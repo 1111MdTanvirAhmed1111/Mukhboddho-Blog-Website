4fd34cd069268ce639b2d3195a2e3fa082df9af1
1111MdTanvirAhmed1111 <freelancertanvir91@gmail.com>
2025-01-10 03:15:05 +0600

    set 'ssg' export to false in next.config.mjs, PostList, and blog page components

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ssg: false, // means build time the pages wont generate static html, only runtime with ISR
   images: {
     remotePatterns: [
       {
